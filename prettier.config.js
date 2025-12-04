c9fdb0d110f50a713ceaa2d6758a7c38750f0b97
sunny <goodluck14115@gmail.com>
2025-12-04 12:22:54 +0530

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+const config = {
+  arrowParens: "avoid",
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "es5",
+  printWidth: 100,
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+};
+
+export default config;
